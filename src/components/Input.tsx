7ec37ab156e229cafd25c54aac34e6f17bc942f9
marlon-clemente <marlon.klemente@gmail.com>
2022-09-15 19:37:53 -0300

    form add

---
+++ src/components/Input.tsx
@@ -0,0 +1,34 @@
+import { useField } from "@unform/core";
+import { useEffect, useRef } from "react";
+
+type InputComponentProps = {
+  name: string;
+};
+
+export default function Input({ name, ...rest }: InputComponentProps) {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef,
+      getValue: (ref) => {
+        return ref.current.value;
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value;
+      },
+      clearValue: (ref) => {
+        ref.current.value = "";
+      },
+    });
+  }, [fieldName, registerField]);
+  return (
+    <input
+      className="py-1 px-2 border-none bg-gray-200 rounded"
+      ref={inputRef}
+      defaultValue={defaultValue}
+      {...rest}
+    />
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,30 @@
+import { Form } from "@unform/web";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
+import Input from "../components/Input";
 
 import { data } from "../data";
 
 const Home: NextPage = () => {
   const [dataItens, setDataItens] = useState<Array<TypeData>>(data);
 
+  const handleSubmit = () => {
+    console.log("ok");
+  };
+
   useEffect(() => {}, []);
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-900">
+      <h1 className="my-24">RENDERMOINHO</h1>
+
       <div className="flex w-full items-center justify-center gap-2">
         {data.map((d: TypeData, indeContentDiv: number) => (
           <div
             key={d.id}
             className="border rounded p-1 flex flex-col items-center bg-gray-600"
           >
-            <text className="my-2 text-white">Pos. {indeContentDiv}</text>
+            <text className="my-2 text-white">Pos. {indeContentDiv + 1}</text>
             <div className="w-16 h-16 rounded flex flex-col bg-gray-400 items-center justify-center">
               <strong>ID #{d.id}</strong>
               <div>Od. {d.orden}</div>
@@ -24,11 +32,31 @@ const Home: NextPage = () => {
           </div>
         ))}
       </div>
+
       <div className="my-4 text-gray-500">
         <strong>Legenda - </strong>
         <strong>ID</strong>: Identificador <strong>Od</strong>: Ordem do
         elemento{" "}
       </div>
+
+      <Form onSubmit={handleSubmit} className="flex flex-col gap-1 my-12">
+        <text className="text-xl text-gray-100 font-bold">
+          Alterar a ordem dos elementos
+        </text>
+
+        <p className="text-md text-gray-300 mt-4">Ordem atual do item (Ord)</p>
+        <Input name="email" />
+
+        <p className="text-md text-gray-300 mt-4">Nova posição (Pos)</p>
+        <Input name="password" />
+
+        <button
+          className="py-2 my-1 p-16 bg-orange-500 rounded text-white hover:bg-orange-300 hover:transition hover:text-gray-800"
+          type="submit"
+        >
+          Salvar
+        </button>
+      </Form>
     </div>
   );
 };
